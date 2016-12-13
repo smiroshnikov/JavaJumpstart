8a231620692d7ee4a0b1065001355fbef6380c92
smiroshnikov <sergeim.qa@gmail.com>
2016-12-13 14:25:37 +0200

    added pez, recall treehouse

---
+++ hitJumpstart/src/Pez_1_0.java
@@ -0,0 +1,5 @@
+public class Pez_1_0 {
+    String characterName = "Yoda";
+}
+
+
--- hitJumpstart/src/Program.java
+++ hitJumpstart/src/Program.java
@@ -1,14 +1,24 @@
 public class Program {
+
     public static void printTextMoreTimes(String s, int count) {
         for (int i = 0; i < count; i++) {
             System.out.println(s);
-
         }
     }
 
     public static void main(String[] args) {
         Rectangle ob = new Rectangle(3, 4);
         System.out.println(ob.area());
+        Pez_1_0 yodaD = new Pez_1_0();
+        Pez_1_0 veiderD = new Pez_1_0();
+
+        yodaD.characterName = "This is fucking Yoda!";
+        veiderD.characterName = "This is Darth Veider";
+        System.out.println("Yoda obj. = " + yodaD.characterName);
+        yodaD.characterName = "Bob, scuko Bob!";
+        System.out.println("Yoda name parameter has changed to -->  = " + yodaD.characterName);
+
+
         printTextMoreTimes("Java is very nice!", 2);
         String fistName = "Sergei";
         boolean isSergei = fistName.equals("Sergei");
@@ -19,5 +29,7 @@ public class Program {
         System.out.println("This is chained method " + someWords.toLowerCase().contains("words"));
         boolean coolExample = false || false || true;
         System.out.println("coolExample is always  " + coolExample);
+
     }
 }
+
