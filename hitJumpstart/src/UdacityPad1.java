90dc5a8d0fba700be39fe7205485ed74182e1ef2
smiroshnikov <sergeim.qa@gmail.com>
2016-12-19 17:40:57 +0200

    Udacity , toying with basics , fixed car quiz

--- hitJumpstart/src/UdacityPad1.java
+++ hitJumpstart/src/UdacityPad1.java
@@ -3,21 +3,18 @@
  */
 public class UdacityPad1 {
     public static void main(String[] args) {
-        Car oldCar = new Car(15, 10);
-        Car sleekCar = new Car(30, 50);
-
-        System.out.printf("sleek CAR gas COST is %d\n", sleekCar.getGas_cost());
-        System.out.printf("old CAR gas COST is %d\n", oldCar.getGas_cost());
-
-        if (oldCar.getPurchasePrice() * oldCar.getGas_cost() < sleekCar.getPurchasePrice() * sleekCar.getGas_cost()) {
+        Car oldCar = new Car(15000, 10);
+        Car sleekCar = new Car(30000, 50);
+        if (oldCar.getPurchasePrice() + oldCar.getGas_cost() < sleekCar.getPurchasePrice() + sleekCar.getGas_cost()) {
             System.out.println("Buy OLD CAR ! ");
-
-            System.out.printf("%d gas cost multiplied by %d$ price ", oldCar.getGas_cost(), oldCar.getPurchasePrice() * oldCar.getGas_cost());
         } else {
             System.out.println("Buy SLEEK CAR ! its cheaper and has a sexy look  ");
-            System.out.printf("I have calculated new car gas cost is %d$ and that is way less than old car gas cost %d$",
-                    sleekCar.getGas_cost(), oldCar.getGas_cost());
         }
-
+        System.out.printf("The whole package for sleek one, including car price and 50,000 miles driving will cost ya %d\n",
+                sleekCar.getGas_cost() + sleekCar.getPurchasePrice());
+        System.out.printf("For comparison buying an old car and driving 50,000 miles will cost ya %d\n",
+                oldCar.getGas_cost() + oldCar.getPurchasePrice());
     }
+
 }
+
