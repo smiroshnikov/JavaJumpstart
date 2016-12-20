6f397f919a753ba9038529cd128c0048e49f1a7d
smiroshnikov <sergeim.qa@gmail.com>
2016-12-20 22:09:11 +0200

    PukanBombit works as designed!

--- hitJumpstart/src/PukanBombit.java
+++ hitJumpstart/src/PukanBombit.java
@@ -3,21 +3,19 @@
  */
 public class PukanBombit {
     public static void main(String[] args) {
-        String myPukan = "PukanBomb$t!";
-        char[] letterList = myPukan.toCharArray();
+        String myPukan = "PukanBo458mb$t";
+        char[] letterList = myPukan.toLowerCase().toCharArray();
 
-        try {
-            for (char letter : letterList) {
-                if (!Character.isLetter(letter) || letter != '$') {
+        for (char letter : letterList) {
+            try {
+                if (!Character.isLetter(letter) && letter != '$') {
                     throw new IllegalArgumentException("Ubeisya Scuko!");
                 }
                 System.out.println(letter);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
         }
         System.out.println("moving on....");
     }
-
-
 }
