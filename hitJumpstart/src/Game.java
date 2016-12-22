92eabbaaee79be13d8502880be1419734f4fae1a
smiroshnikov <sergeim.qa@gmail.com>
2016-12-22 21:24:54 +0200

    Important practices and comments

--- hitJumpstart/src/Game.java
+++ hitJumpstart/src/Game.java
@@ -4,8 +4,8 @@
 
 public class Game {
     public static final int MAX_MISSES = 7;
-    private String answer;
-    private String hits;
+    private final String answer;
+    private String hits; // should be stringbuilders for optimization purposes
     private String misses;
 
     public String getAnswer() {
@@ -22,10 +22,12 @@ public class Game {
         misses = "";
     }
 
-    public boolean applyGuess(char letter) {
+    public boolean applyGuess(char letter) { // try to use exceptions only when necessary , slows performance!
+        // IllegalArgumentException should be used in constructor , 99% of Java devs do this , should be documented
+        // will prevent incorrect / invalid object instantiation
         letter = normalizedGuess(letter);
         boolean isHit = answer.indexOf(letter) != -1; // this is a weird way to search...NOT not found
-        if (isHit) {
+        if (isHit) { // good practice is to evade such simple variables , can be used for better readability
             hits += letter;
             // Debug messages
             //System.out.printf("hits are %s %n ", hits);
@@ -38,6 +40,7 @@ public class Game {
     }
 
     public boolean applyGuess(String letters) {
+        // reusing method is also very good , only 1 place to add functionality and fix errors
         if (letters.isEmpty()) {
             throw new IllegalArgumentException("Empty Input ! ");
         }
@@ -48,18 +51,21 @@ public class Game {
     // I can accept strings and empty strings as input
 
     public String getCurrentProgress() {
-        String progress = "";
+        StringBuilder stringBuilder = new StringBuilder();
         for (char letter : answer.toCharArray()) {
-            char display = '_';
-            if (hits.indexOf(letter) != -1) {
-                display = letter;
+            if (hits.indexOf(letter) == -1) {
+                letter = '_';  // SHOULD BE CONST , HARDCODED VALUE , object is destroyed right after creation
+                // optimized with CONST , and also will provide a single place to change
             }
-            progress += display;
+            stringBuilder.append(letter); // string concatenation is very ineffective , creating a lot of object that are
+            // destroyed right after usage. If I  had 50 string concatenated in similar way  49 objects would be
+            // ineffective and highly overloading garbage collector, replaced with stringbuilder instance
         }
-        return progress;
+        return stringBuilder.toString();
     }
 
-    private char normalizedGuess(char letter) {
+    private char normalizedGuess(char letter) { // not related to Game logic , this function should be in prompter
+        //  separate functionality lowerCase and and user error , not reusable !
         if (!Character.isLetter(letter)) {
             throw new IllegalArgumentException("Fuck you ! This is not a letter !");
         }
@@ -72,5 +78,5 @@ public class Game {
 
     public boolean isWon() {
         return getCurrentProgress().indexOf('_') == -1;
-    }
+    } // CONST !!!
 }
--- hitJumpstart/src/Prompter.java
+++ hitJumpstart/src/Prompter.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 /**
  * Created by Iidwuurliik on 12/17/2016.
  */
-public class Prompter {
+public class Prompter { // Prompter can be reused , in order to achieve that I should separate I/O
+    // for example I could decide use 3 inputs and 1 output , in that case I need to rewrite this class or create a new
+    // one with almost same code
     // handles IO
-    private Game game;
+    private final Game game; // Game doesnt change during prompter
 
     public Prompter(Game game) {
         // private variable is equal to argument that came in
@@ -15,15 +17,18 @@ public class Prompter {
     public boolean promptForGuess() {
         boolean isHit = false;
         boolean isAcceptable = false;
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Scanner should be global and initialized in constructor
+        // there is also no need to instantiate a new scanner object each time when this function is running
+        // variable that without it class cannot work or exist should be in constructor
         do {
+            // do while input is acceptable
             System.out.print("Please guess a letter : ");
             String guessInput = scanner.nextLine();
             //char guess = guessInput.toLowerCase().charAt(0); //solved by myself
             //char guess = guessInput.charAt(0); - no longer needed !
             try {
                 isHit = game.applyGuess(guessInput);
-                isAcceptable = true;
+                isAcceptable = true; // break can and should be used here
             } catch (IllegalArgumentException iae) {
                 System.out.printf("%s .Please try again %n", iae.getMessage());
             }
@@ -37,7 +42,9 @@ public class Prompter {
         // TODO game done in python 2 years ago
     }
 
-    public void displayProgress() {
+    public void displayProgress() { // should be in Game , can be replaced with Prompter function displayTries will
+        // receive game as argument .
+        // game with calculate a progress
         if (!game.isWon()) {
             System.out.printf("You have %d tries , to solve : %s%n", game.getRemainingTries(), game.getCurrentProgress());
         }
