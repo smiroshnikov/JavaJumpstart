6bf56c10a1a05e572fa370381e93b0c4a4da9a0a
smiroshnikov <sergeim.qa@gmail.com>
2016-12-22 17:38:48 +0200

    Treet , Date , concept explanation

--- hitJumpstart/src/Thing.java
+++ hitJumpstart/src/Thing.java
@@ -1,6 +1,6 @@
-import com.teamtreehouse.Treet;
-
 import java.util.Date;
+
+import com.teamtreehouse.Treet;
 
 /**
  * Created by Iidwuurliik
@@ -10,10 +10,13 @@ public class Thing {
     public static void main(String[] args) {
         Date date = new Date(2016, 11, 11);
         Date date2 = new Date();
-        Treet treet = new Treet("Momo", "Free Penis Enlargement !", date);
+        Treet treet = new Treet(
+                "Momo",
+                "Free Penis Enlargement !",
+                new Date(1482421000000L)); // don't forget about this , instantiated during other
+        // class instantiation , also "L" for milliseconds from epoch
+
 
         System.out.println(treet.getDateWasSet());
-
-
     }
 }
