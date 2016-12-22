59ed34757bf7be0078a2381509fafc12c44bcd49
smiroshnikov <sergeim.qa@gmail.com>
2016-12-22 18:57:01 +0200

    object casting

--- hitJumpstart/src/Thing.java
+++ hitJumpstart/src/Thing.java
@@ -1,6 +1,7 @@
 import java.util.Date;
 
 import com.teamtreehouse.Treet;
+import sun.reflect.generics.tree.Tree;
 
 /**
  * Created by Iidwuurliik
@@ -18,5 +19,22 @@ public class Thing {
         System.out.println(treet.getDateWasSet());
         System.out.println(treet.toString());
         System.out.printf("%s", treet); // this is very very interesting , printf calls automatically for overriden toString class ?!
+
+        Object obj = treet;
+        System.out.printf("%n Upcast ?! %s %n", obj.toString()); // Max
+        Treet another = (Treet) obj; // downcasting ?
+        System.out.printf("Downcasting %s %n", another);
+        Object[] someStuff = {treet, "whatever String"};
+        System.out.println(((Treet) someStuff[0]).getDescription()); // actually works!
+        //System.out.println(((Treet) someStuff[1]).getDescription()); // will fail ! need more info on downcasting
+        System.out.println(someStuff[0] instanceof Treet);
+
+        System.out.println(someStuff[1] instanceof Treet);
+
+        System.out.println(someStuff[0] instanceof String);
+
+        System.out.println(someStuff[1] instanceof String);
+
+
     }
 }
