cc46e576d1898d56832643ed3c1e33273b2a8d46
smiroshnikov <sergeim.qa@gmail.com>
2016-12-26 23:16:43 +0200

    TTH Strings arrays lesson 3

--- hitJumpstart/src/StringManipulation.java
+++ hitJumpstart/src/StringManipulation.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // Stuck with regex ? use visual regex online ! Fuck understanding ! Easy...
 public class StringManipulation {
     public static void main(String[] args) {
@@ -8,18 +10,29 @@ public class StringManipulation {
             System.out.println(i);
         }
         String[] items = new String[10]; // OK declaration
-        items[0] = "blade";
+        items[0] = "mace"; //  not effective , passing array with nulls inside
         items[1] = "hammer";
-        items[2] = "mace";
+        items[2] = "blade";
 
-        for (String item : items
-                ) {
-            if (item != null) {
-                System.out.println(item);
-            }
-
-
+        String[] extendedWeaponsList = new String[4];
+        System.arraycopy(items, 0, extendedWeaponsList, 0, 4);
+        for (String element : extendedWeaponsList) {
+            System.out.println(element);
         }
+        extendedWeaponsList[3] = "spear";
+        // a bot more affective approach ?
+        String[] secondArray = Arrays.copyOf(extendedWeaponsList, 12);
+
+        Arrays.sort(extendedWeaponsList); // array with null will not be be sorted
+
+        for (String e : secondArray
+                ) {
+            System.out.println(e);
+        }
+        String[] bullshit = {"Larry", "Bob"};
+        bullshit = Arrays.copyOf(bullshit, 3);
+        System.out.println("Here ! ");
+        System.out.println(bullshit[2]);
 
 
     }
